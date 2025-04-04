f3ad6d6e374b66ca442acac1c0a5a22f29bd6e10
Mcarre027 <carrehomebusiness@gmail.com>
2025-04-04 10:29:23 +0200

    modification navbar

--- app.py
+++ app.py
@@ -1,28 +1,34 @@
 import dash
 from dash import html
 import dash_bootstrap_components as dbc
+from dash_iconify import DashIconify
 
-# Initialisation de l'app
 app = dash.Dash(__name__, use_pages=True, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-print(dash.page_registry.keys())
 app.title = "Effets secondaires des vaccins"
 
-# Layout global
+navbar = dbc.NavbarSimple(
+    brand=[
+        DashIconify(icon="fa-solid:virus", width=24, style={"margin-right": "10px"}),
+        html.Span("Effets secondaires des vaccins")
+    ],
+    brand_href="/",
+    color="#1e2130",
+    dark=True,
+    children=[
+        dbc.NavItem(dbc.NavLink("Accueil", href="/")),
+        dbc.NavItem(dbc.NavLink("Heatmap", href="/heatmap")),
+        dbc.NavItem(dbc.NavLink("Exploration", href="/exploration")),
+    ],
+    className="mb-4"
+)
+
 app.layout = dbc.Container([
+    navbar,
     dbc.Row([
-        dbc.Col(html.H1("Effets secondaires des vaccins", className="text-center my-4"))
-    ]),
-    dbc.Row([
-        dbc.Col(dbc.Nav([
-            dbc.NavLink("Accueil", href="/", active="exact"),
-            dbc.NavLink("Heatmap", href="/heatmap", active="exact"),
-            dbc.NavLink("Exploration", href="/exploration", active="exact")
-        ], vertical=True, pills=True), width=2),
-        dbc.Col(dash.page_container, width=10)
+        dbc.Col(dash.page_container)
     ])
 ], fluid=True)
 
 if __name__ == "__main__":
-    app.run (debug=False, host="0.0.0.0", port=8000)
-
+    app.run(debug=False, host="0.0.0.0", port=8000)
